bf400b8be901afa78a718f281a987b3963a97e08
rkfcl <kwmkr6@gmail.com>
2024-10-31 14:17:43 +0900

    feat: update rds_endpoint

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "this" {
 
 # EC2 인스턴스를 타겟 그룹에 등록하는 리소스 추가
 resource "aws_lb_target_group_attachment" "this" {
-  target_group_arn = var.target_group_arn != null ? var.target_group_arn : null  # 타겟 그룹 ARN을 변수로 지정
-  target_id        = aws_instance.this.id    # 생성된 EC2 인스턴스 ID
+  count            = var.target_group_arn != null ? 1 : 0   # target_group_arn이 null이 아닐 때만 리소스 생성
+  target_group_arn = var.target_group_arn                   # 타겟 그룹 ARN을 변수로 지정
+  target_id        = aws_instance.this.id                   # 생성된 EC2 인스턴스 ID
 }
